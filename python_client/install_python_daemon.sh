04626f21381d0d0975d649db1a2306c0e3eccd46
Ubuntu <vmadmin@synthea-host.ip4dmbsa5isudj4aoeu3pt14zb.cx.internal.cloudapp.net>
2022-03-08 16:55:46 +0000

    Updated deployment scripts

---
+++ python_client/install_python_daemon.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+connection_string=$1		
+polling_interval=$2		
+FHIR_output_path=$3		
+local_output_path=$4		
+log_path=$5		
+container_name=$6		
+
+git clone https://github.com/jbinko/PythonSyntheaFHIRClient.git
+cd ./PythonSyntheaFHIRClient/python_client
+
+echo "Writing config file..."
+
+content=$'{\n\"connection_string\":\"'"${connection_string}"$'\",\n\"polling_interval\":\"'"${polling_interval}"$'\",\n\"FHIR_output_path\":\"'"${FHIR_output_path}"$'\",\n\"local_output_path\":\"'"${local_output_path}"$'\",\n\"log_path\":\"'"${log_path}"$'\",\n\"container_name\":\"'"${container_name}"$'\"\n}' 
+
+echo "$content" > deploy_config.json
+
---
+++ python_client/setup_environment.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install python3-pip -y
+
+sudo apt install default-jdk
+
+sudo pip3 install azure-storage-blob

e7e277ac53b95e363361d7dce240dfd0cf1d7cc3
DJ Dean <dade@microsoft.com>
2022-03-22 12:51:31 -0400

    Added params to deploy.sh

--- deployment/scripts/deploy.sh
+++ deployment/scripts/deploy.sh
@@ -1,10 +1,17 @@
 #/bin/bash
 #####Clone the git repo#############
 git clone https://github.com/jbinko/PythonSyntheaFHIRClient.git
+cd ./PythonSyntheaFHIRClient/deployment
 #####Run the environment setup script##########
 ./setup_environment.sh
 #####Build and install Synthea###########
 ./install_synthea.sh
 #####Build and install the python deamon##########
-./install_python_client.sh
+connection_string=$1		#Azure Storage account connection string to upload the data to 
+polling_interval=$2		#The rate at which to check for new data
+FHIR_output_path=$3		#The local directory to check for FHIR bundles
+local_output_path=$4		#The local path to move uploaded FHIR data to after being stored in Azure
+log_path=$5			#The path to send the log file to
+container_name=$6		#The container name to upload the data to in the Azure Storage account
+./install_python_client.sh $connection_string $polling_interval $FHIR_output_path $local_output_path $log_path $container_name
 
--- deployment/scripts/install_python_daemon.sh
+++ deployment/scripts/install_python_daemon.sh
@@ -1,16 +1,14 @@
 #/bin/bash
-connection_string=$1		
-polling_interval=$2		
-FHIR_output_path=$3		
-local_output_path=$4		
-log_path=$5		
-container_name=$6		
-
-cd ./PythonSyntheaFHIRClient/python_client
-
+connection_string=$1		#Azure Storage account connection string to upload the data to 
+polling_interval=$2		#The rate at which to check for new data
+FHIR_output_path=$3		#The local directory to check for FHIR bundles
+local_output_path=$4		#The local path to move uploaded FHIR data to after being stored in Azure
+log_path=$5			#The path to send the log file to
+container_name=$6		#The container name to upload the data to in the Azure Storage account
+###Write the config file for the deamon###
 echo "Writing config file..."
 
 content=$'{\n\"connection_string\":\"'"${connection_string}"$'\",\n\"polling_interval\":\"'"${polling_interval}"$'\",\n\"FHIR_output_path\":\"'"${FHIR_output_path}"$'\",\n\"local_output_path\":\"'"${local_output_path}"$'\",\n\"log_path\":\"'"${log_path}"$'\",\n\"container_name\":\"'"${container_name}"$'\"\n}' 
 
-echo "$content" > deploy_config.json
+echo "$content" > ../PythonSyntheaFHIRClient/python_client/deploy_config.json
 

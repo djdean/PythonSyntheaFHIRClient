08f400ecbefc14b7d5ec84d4abd76308879813c2
jbinko <jbinko+github@gmail.com>
2022-03-28 22:15:50 +0200

    Update deploy.sh

--- deployment/scripts/deploy.sh
+++ deployment/scripts/deploy.sh
@@ -7,10 +7,10 @@ cd /home/synthea
 git clone https://github.com/djdean/PythonSyntheaFHIRClient.git
 #####Run the environment setup script##########
 chmod 700 ./PythonSyntheaFHIRClient/deployment/scripts/setup_environment.sh
-./PythonSyntheaFHIRClient/deployment/scripts/setup_environment.sh
+sudo ./PythonSyntheaFHIRClient/deployment/scripts/setup_environment.sh
 #####Build and install Synthea###########
 chmod 700 ./PythonSyntheaFHIRClient/deployment/scripts/install_synthea.sh
-./PythonSyntheaFHIRClient/deployment/scripts/install_synthea.sh
+sudo ./PythonSyntheaFHIRClient/deployment/scripts/install_synthea.sh
 #####Build and install the python deamon##########
 connection_string=$1		#Azure Storage account connection string to upload the data to 
 polling_interval=$2		#The rate at which to check for new data
@@ -19,5 +19,5 @@ local_output_path=$4		#The local path to move uploaded FHIR data to after being 
 log_path=$5			#The path to send the log file to
 container_name=$6		#The container name to upload the data to in the Azure Storage account
 chmod 700 ./PythonSyntheaFHIRClient/deployment/scripts/install_python_daemon.sh
-./PythonSyntheaFHIRClient/deployment/scripts/install_python_daemon.sh $connection_string $polling_interval $FHIR_output_path $local_output_path $log_path $container_name
+sudo ./PythonSyntheaFHIRClient/deployment/scripts/install_python_daemon.sh $connection_string $polling_interval $FHIR_output_path $local_output_path $log_path $container_name
 echo "Done!"

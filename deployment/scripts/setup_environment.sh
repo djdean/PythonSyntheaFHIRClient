561a4a1bd7da431206704c3959961bf1e13c2ca0
jbinko <jbinko+github@gmail.com>
2022-04-03 21:17:29 +0200

    openjdk-17-jre-headless

--- deployment/scripts/setup_environment.sh
+++ deployment/scripts/setup_environment.sh
@@ -4,6 +4,6 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install python3-pip -y
 
-sudo apt install default-jdk
+sudo apt install openjdk-17-jre-headless -y
 
-sudo pip3 install azure-storage-blob
+sudo pip3 install azure-storage-blob -y

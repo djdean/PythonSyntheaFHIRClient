a6b051f0925cc6f5f1729fec94bf8a20dc3474da
jbinko <jbinko+github@gmail.com>
2022-04-03 21:42:24 +0200

    Update setup_environment.sh

--- deployment/scripts/setup_environment.sh
+++ deployment/scripts/setup_environment.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
-sudo apt update
+sudo apt update -y
 sudo apt upgrade -y
 sudo apt install python3-pip -y
 
 sudo apt install openjdk-17-jre-headless -y
 
-sudo pip3 install azure-storage-blob -y
+sudo pip3 install azure-storage-blob

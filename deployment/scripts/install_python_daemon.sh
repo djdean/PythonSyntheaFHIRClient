5e7a52f18dc43ce30bf5685b0a2ae63544593c5c
jbinko <jbinko+github@gmail.com>
2022-04-03 23:58:16 +0200

    Update install_python_daemon.sh

--- deployment/scripts/install_python_daemon.sh
+++ deployment/scripts/install_python_daemon.sh
@@ -8,6 +8,6 @@ container_name=$6		#The container name to upload the data to in the Azure Storag
 ###Write the config file for the deamon###
 echo "Writing config file..."
 
-content=$'{\n\"connection_string\":\"'"${connection_string}"$'\",\n\"polling_interval\":\"'"${polling_interval}"$'\",\n\"FHIR_output_path\":\"'"${FHIR_output_path}"$'\",\n\"local_output_path\":\"'"${local_output_path}"$'\",\n\"log_path\":\"'"${log_path}"$'\",\n\"container_name\":\"'"${container_name}"$'\"\n}' 
+content=$'{\n"connection_string":"'"${connection_string}"$'",\n"polling_interval":"'"${polling_interval}"$'",\n"FHIR_output_path":"'"${FHIR_output_path}"$'",\n"local_output_path":"'"${local_output_path}"$'",\n"log_path":"'"${log_path}"$'",\n"container_name":"'"${container_name}"$'"\n}'
 
 echo "$content" > ./PythonSyntheaFHIRClient/python_client/deploy_config.json

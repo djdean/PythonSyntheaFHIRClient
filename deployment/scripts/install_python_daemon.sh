bb94a1339a33aac58f28d1a939b8603ee7b22148
jbinko <jbinko+github@gmail.com>
2022-04-04 17:54:49 +0200

    Update install_python_daemon.sh

--- deployment/scripts/install_python_daemon.sh
+++ deployment/scripts/install_python_daemon.sh
@@ -9,15 +9,15 @@ container_name=$6		#The container name to upload the data to in the Azure Storag
 ###Write the config file for the deamon###
 echo "Writing config file..."
 
-content_string="{"$'\n'
-
-content_string+=$(printf '%s' $'\t'"\"connection_string\":\"$connection_string\"",)$'\n'
-content_string+=$(printf '%s' $'\t'"\"polling_interval\":\"$polling_interval\"",)$'\n'
-content_string+=$(printf '%s' $'\t'"\"FHIR_output_path\":\"$FHIR_output_path\"",)$'\n'
-content_string+=$(printf '%s' $'\t'"\"local_output_path\":\"$local_output_path\"",)$'\n'
-content_string+=$(printf '%s' $'\t'"\"log_path\":\"$log_path\"",)$'\n'
-content_string+=$(printf '%s' $'\t'"\"container_name\":\"$container_name\"")$'\n'
-
+content_string="{"$'\n' \
+\
+content_string+=$(printf '%s' $'\t'"\"connection_string\":\"$connection_string\"",)$'\n' \
+content_string+=$(printf '%s' $'\t'"\"polling_interval\":\"$polling_interval\"",)$'\n' \
+content_string+=$(printf '%s' $'\t'"\"FHIR_output_path\":\"$FHIR_output_path\"",)$'\n' \
+content_string+=$(printf '%s' $'\t'"\"local_output_path\":\"$local_output_path\"",)$'\n' \
+content_string+=$(printf '%s' $'\t'"\"log_path\":\"$log_path\"",)$'\n' \
+content_string+=$(printf '%s' $'\t'"\"container_name\":\"$container_name\"")$'\n' \
+ \
 content_string+="}"
 
 

5cf89550751a2efa8e8c2a52ff528e97a0ee318a
DJ Dean <dade@microsoft.com>
2022-04-04 10:26:42 -0400

    Fixed output formattting errors

--- deployment/scripts/install_python_daemon.sh
+++ deployment/scripts/install_python_daemon.sh
@@ -5,9 +5,20 @@ FHIR_output_path=$3		#The local directory to check for FHIR bundles
 local_output_path=$4		#The local path to move uploaded FHIR data to after being stored in Azure
 log_path=$5			#The path to send the log file to
 container_name=$6		#The container name to upload the data to in the Azure Storage account
+
 ###Write the config file for the deamon###
 echo "Writing config file..."
 
-content=$'{\n\"connection_string\":\"'"${connection_string}"'\",\n\"polling_interval\":\"'"${polling_interval}"'\",\n\"FHIR_output_path\":\"'"${FHIR_output_path}"'\",\n\"local_output_path\":\"'"${local_output_path}"'\",\n\"log_path\":\"'"${log_path}"'\",\n\"container_name\":\"'"${container_name}"'\"\n}'
+content_string="{"$'\n'
 
-echo "$content" > ./PythonSyntheaFHIRClient/python_client/deploy_config.json
+content_string+=$(printf '%s' $'\t'"\"connection_string\":\"$connection_string\"",)$'\n'
+content_string+=$(printf '%s' $'\t'"\"polling_interval\":\"$polling_interval\"",)$'\n'
+content_string+=$(printf '%s' $'\t'"\"FHIR_output_path\":\"$FHIR_output_path\"",)$'\n'
+content_string+=$(printf '%s' $'\t'"\"local_output_path\":\"$local_output_path\"",)$'\n'
+content_string+=$(printf '%s' $'\t'"\"log_path\":\"$log_path\"",)$'\n'
+content_string+=$(printf '%s' $'\t'"\"container_name\":\"$container_name\"")$'\n'
+
+content_string+="}"
+
+
+echo "$content_string" > deploy_config.json
